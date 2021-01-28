c57b47434eb79afa4adb95f6179fd729998086e8
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2021-01-27 20:29:02 -0500

    Initial commit

---
+++ ShipBattles/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShipBattles
+{
+    class Board
+    {
+        private readonly int[] SHIP_SIZES = new int[]{5, 4, 3, 3, 2}; // how big each ship is
+        private readonly string[] SHIP_NAMES = new string[]{"Carrier", // the names of each ship, matches respectively to sizes
+        "Battleship", "Cruiser", "Submarine", "Destroyer"};
+        private string[,] boardVals = new string[10, 10];
+
+
+        public Board()
+        {
+            // put an empty space in each spot on the board as it's created
+            for (int i = 0; i < boardVals.GetLength(0); i++)
+            {
+                for (int n = 0; n < boardVals.GetLength(1); n++)
+                {
+                    // assign " "
+                    boardVals[i, n] = " ";
+                }
+            }
+
+
+
+        }
+
+        // Get the array from the board
+        public string[,] GetBoardVals()
+        {
+            return boardVals;
+        }
+
+    }
+}
---
+++ ShipBattles/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ShipBattles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            Board playerShipBoard = new Board(); // the board the player will place ships on
+            Board playerGuessBoard = new Board(); // the board where the player places their guesses
+            Board compShipBoard = new Board(); // the board the computer will place ships on
+            Board compGuessBoard = new Board(); // the board where the computer will guess
+
+            // test the board display
+            //DisplayBoard(playerShipBoard);
+            DisplayBoth(playerGuessBoard, playerShipBoard);
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+        // Display a board
+        public static void DisplayBoard(Board board)
+        {
+            // variables
+            string[,] boardVals = board.GetBoardVals();
+            string letters = "    A  B  C  D  E  F  G  H  I  J\n";
+
+            // first print a line of "-"
+            //Console.WriteLine(line);
+
+            // interate through the full board
+            for (int i = 0; i < boardVals.GetLength(0); i++)
+            {
+                if (i == 0)
+                {
+                    // write the initial |
+                    Console.Write(letters);
+                    //Console.Write("\n|");
+                }
+
+                for (int n = 0; n < boardVals.GetLength(1); n++)
+                {
+
+                    // if it's the first value in the line, do this
+                    if (n == 0)
+                    {
+                        if (i != 9)
+                        {
+                            // write the initial |
+                            Console.Write(" " + (i + 1) + " ");
+                            //Console.Write("\n|");
+                        } else
+                        {
+                            // write the initial |
+                            Console.Write((i + 1) + " ");
+                            //Console.Write("\n|");
+                        }
+
+                    }
+
+                    // otherwise write the value and then |
+                    Console.Write("[" + boardVals[i, n] + "]");
+
+                    // if it's the last value in the line, do this
+                    
+                }
+
+                // write another line of "-" after a the line of values is finished
+                Console.Write("\n");
+            }
+
+            // test
+            // ---------------------
+            // | | | | | | | | | | |
+        }
+
+        // Display both boards on your own side
+        public static void DisplayBoth(Board guessBoard, Board shipBoard)
+        {
+            string line = "---------------------------------";
+
+            DisplayBoard(guessBoard);
+            Console.WriteLine(line);
+            DisplayBoard(shipBoard);
+
+        }
+
+    }
+}
