c0cd2535604bb97021644c99e2e34e13ae39aae9
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2021-01-28 16:06:11 -0500

    Added methods to validate position input from user.

--- ShipBattles/Board.cs
+++ ShipBattles/Board.cs
@@ -9,6 +9,8 @@ namespace ShipBattles
         private readonly int[] SHIP_SIZES = new int[]{5, 4, 3, 3, 2}; // how big each ship is
         private readonly string[] SHIP_NAMES = new string[]{"Carrier", // the names of each ship, matches respectively to sizes
         "Battleship", "Cruiser", "Submarine", "Destroyer"};
+        private readonly string[] letters = new string[] { "A", "B", "C", "D", // letters on the board
+                "E", "F", "G", "H", "I", "J" };
         private string[,] boardVals = new string[10, 10];
 
 
@@ -31,6 +33,43 @@ namespace ShipBattles
         {
             return boardVals;
         }
+
+
+        // Add a ship to the board
+        public bool AddShip()
+        {
+            // this returns a boolean that tells if adding it was successful or not
+            // NOTE testing for correct letter input must be done in the main program
+
+            // variables
+            bool successful = true; // sets to false if a space is already taken
+
+
+            return successful; // return whether it was successful
+
+        }
+
+        // returns the number positions on a board based on a user's input
+        // NOTE the input must be in this format "A1"
+        public int[] MatchSpace(string space)
+        {
+            // variables
+            int[] spots = new int[2];
+
+            try
+            {
+
+            } catch (Exception)
+            {
+                Console.WriteLine("Error: could not match position on board.\nReturning empty array.");
+            }
+
+            return spots; // return result
+
+        }
+
+
+
 
         // Generate random positions of ships for an AI board
         public void GeneratePositions()
@@ -156,12 +195,12 @@ namespace ShipBattles
 
                         Console.WriteLine("\nTesting for empty spots."); // test
 
-                        for (int x = 0; x < shipSize; x++)
+                        for (int n = 0; n < shipSize; n++)
                         {
-                            Console.WriteLine($"Index: {positionsX[x]},{positionsY[x]}"); // test
+                            Console.WriteLine($"Index: {positionsX[n]},{positionsY[n]}"); // test
 
                             // check if that position on the board is empty
-                            if (!boardVals[positionsX[x], positionsY[x]].Equals(" "))
+                            if (!boardVals[positionsX[n], positionsY[n]].Equals(" "))
                             {
                                 Console.WriteLine("Not empty. Not a valid ship position."); // test
                                 valid = false;
@@ -191,261 +230,6 @@ namespace ShipBattles
 
             }
         }
-
-                
-
-
-                /*
-                // variables
-                bool valid = true; // determines if these are valid positions for that ship
-                int posX;
-                int posY;
-                int direction; // 1 is left, 2 is right, 3 is up, 4 is down
-                Random random = new Random();
-
-                // generate a random position for the first spot on the ship
-                do // if valid remains true, break, otherwise find a different position
-                {
-                    // variables
-                    bool validDir = true; // determine if it's going in a valid direction without running into problems
-
-                    // generate position
-                    posX = random.Next(0, 10);
-                    posY = random.Next(0, 10);
-
-                    // check if that position is taken or not
-                    if (boardVals[posX, posY] != " ") // if it's not " " then it's taken
-                    {
-                        valid = false; // it's no longer valid if this is the case so it will generate a new position
-                        Console.WriteLine("Not valid"); // test
-                    }
-
-                    // if it's still valid, generate a direction
-                    if (valid) // if it's still alid, continue
-                    {
-                        do
-                        {
-
-                            // generate a direction
-                            direction = random.Next(1, 5); // determine which direction to go
-
-                            // determine if all items in that direction are blank, or within range
-                            // decide which way it's going though first
-                            if (direction == 1) // left
-                            {
-                                int tempPosX = posX;
-                                int tempPosY = posY;
-
-
-                                // go through positions for that ship
-                                for (int i = 0; i < SHIP_SIZES[ship]; i++)
-                                {
-                                    // test
-                                    Console.WriteLine("Ship size: " + SHIP_SIZES[ship]);
-
-
-                                    // see if it's less than 0
-                                    if (tempPosX < 0)
-                                    {
-                                        validDir = false; // if it's not in the grid, valid is false
-                                    }
-
-                                    // check if it's taken or not, and if it's still valid
-                                    if (validDir)
-                                    {
-                                        if (boardVals[tempPosX, tempPosY] != " ")
-                                        {
-                                            Console.WriteLine("Positions: " + tempPosX + " " + tempPosY); // test
-                                            validDir = false; // if the space is taken or off the board, it's not valid
-                                        }
-                                            
-                                    }
-
-                                    // if it's still valid, keep checking, otherwise break
-                                    if (validDir)
-                                    {
-                                        // change the pos
-                                        tempPosX -= 1;
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-                                    
-                                }
-                                
-
-                            } else if (direction == 2) // right
-                            {
-                                int tempPosX = posX;
-                                int tempPosY = posY;
-
-
-                                // go through positions for that ship
-                                for (int i = 0; i < SHIP_SIZES[ship]; i++)
-                                {
-                                    // see if it's greater than 9
-                                    if (tempPosX > 9)
-                                    {
-                                        validDir = false; // if it's not in the grid, valid is false
-                                    }
-
-                                    // check if it's taken or not, and if it's still valid
-                                    Console.WriteLine("Positions: " + tempPosX + " " + tempPosY); // test
-                                    if (validDir && !boardVals[tempPosX, tempPosY].Equals(" "))
-                                    {
-                                        validDir = false; // if the space is taken or off the board, it's not valid
-                                    }
-
-                                    // if it's still valid, keep checking, otherwise break
-                                    if (validDir)
-                                    {
-                                        // change the pos
-                                        tempPosX += 1;
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-
-                                }
-
-                            } else if (direction == 3) // up 
-                            {
-                                int tempPosX = posX;
-                                int tempPosY = posY;
-
-
-                                // go through positions for that ship
-                                for (int i = 0; i < ship; i++)
-                                {
-                                    // see if it's less than 0
-                                    if (tempPosY < 0)
-                                    {
-                                        validDir = false; // if it's not in the grid, valid is false
-                                    }
-
-                                    // check if it's taken or not, and if it's still valid
-                                    if (validDir )
-                                    {
-                                        if (boardVals[tempPosX, tempPosY].Equals(" "))
-                                        {
-                                            Console.WriteLine("Positions: " + tempPosX + " " + tempPosY); // test
-
-                                            validDir = false; // if the space is taken or off the board, it's not valid
-                                        }
-                                        
-                                    }
-
-                                    // if it's still valid, keep checking, otherwise break
-                                    if (validDir)
-                                    {
-                                        // change the pos
-                                        tempPosY -= 1;
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-
-                                }
-
-                            } else if (direction == 4) // down
-                            {
-                                int tempPosX = posX;
-                                int tempPosY = posY;
-
-
-                                // go through positions for that ship
-                                for (int i = 0; i < ship; i++)
-                                {
-
-
-                                    // see if it's greater than 9
-                                    if (tempPosY > 9)
-                                    {
-                                        validDir = false; // if it's not in the grid, valid is false
-                                    }
-
-                                    // check if it's taken or not, and if it's still valid
-                                    if (validDir && !boardVals[tempPosX, tempPosY].Equals(" "))
-                                    {
-                                        if (boardVals[tempPosX, tempPosY] != " ")
-                                        {
-                                            Console.WriteLine("Positions: " + tempPosX + " " + tempPosY); // test
-                                            validDir = false; // if the space is taken or off the board, it's not valid
-                                        }
-                                    }
-
-                                    // if it's still valid, keep checking, otherwise break
-                                    if (validDir)
-                                    {
-                                        // change the pos
-                                        tempPosY += 1;
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-
-                                }
-                            }
-
-                            // if validDir is still true, then it's valid, so set those positions
-                            if (validDir)
-                            {
-                                for (int i = 0; i < ship; i++)
-                                {
-                                    int tempPosX = posX;
-                                    int tempPosY = posY;
-
-                                    if (direction == 1)
-                                    {
-                                        
-                                        // loop through ship size and set those positions to +
-                                        boardVals[posX, posY] = "+";
-
-                                        posX -= 1;
-                                    } 
-                                    else if (direction == 2)
-                                    {
-
-                                        // loop through ship size and set those positions to +
-                                        boardVals[posX, posY] = "+";
-
-                                        posX += 1;
-                                    }
-                                    else if (direction == 3)
-                                    {
-
-                                        // loop through ship size and set those positions to +
-                                        boardVals[posX, posY] = "+";
-
-                                        posY -= 1;
-                                    }
-                                    else if (direction == 4)
-                                    {
-
-                                        // loop through ship size and set those positions to +
-                                        boardVals[posX, posY] = "+";
-
-                                        posY += 1;
-                                    }
-                                }
-                            }
-                            
-
-
-                        } while (!validDir);
-                        
-                    }
-
-
-                } while (!valid);
-
-            }
-        }
-                */
 
     }
 }
--- ShipBattles/Program.cs
+++ ShipBattles/Program.cs
@@ -4,6 +4,7 @@ namespace ShipBattles
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             // Variables
@@ -11,6 +12,7 @@ namespace ShipBattles
             Board playerGuessBoard = new Board(); // the board where the player places their guesses
             Board compShipBoard = new Board(); // the board the computer will place ships on
             Board compGuessBoard = new Board(); // the board where the computer will guess
+
 
             // test the board display
             //DisplayBoard(playerShipBoard);
@@ -20,6 +22,12 @@ namespace ShipBattles
             compShipBoard.GeneratePositions();
             Console.WriteLine("\nTesting the board generator for the computer.");
             DisplayBoard(compShipBoard);
+
+            // test getting input from user for a position
+            string pos = AskPosition();
+
+            // test setting that position on the user's board
+
 
             Console.WriteLine("Hello World!");
         }
@@ -31,8 +39,8 @@ namespace ShipBattles
         public static void DisplayBoard(Board board)
         {
             // variables
-            string[,] boardVals = board.GetBoardVals();
             string letters = "    A  B  C  D  E  F  G  H  I  J\n";
+            string[,] boardVals = board.GetBoardVals();
 
             // first print a line of "-"
             //Console.WriteLine(line);
@@ -92,6 +100,89 @@ namespace ShipBattles
             Console.WriteLine(line);
             DisplayBoard(shipBoard);
 
+        }
+
+        // enter a letter, validate it and return that string with correct input
+        public static string AskPosition()
+        {
+            // test the position validator - reuse this to get input
+            bool validEntry = true;
+            string input;
+            do
+            {
+                // get input for position
+                Console.Write("\nInput position: ");
+                input = Console.ReadLine();
+                validEntry = ValidatePosition(input);
+                // tell them it's invalid if it's not valid
+                if (!validEntry)
+                {
+                    Console.Write("Invalid Entry.");
+                }
+            } while (!validEntry);
+
+            return input;
+        }
+
+        // Validate the input from the user for a position - returns true if valid
+        public static bool ValidatePosition(string position)
+        {
+            // variables
+            string[] letters = new string[] { "A", "B", "C", "D", // letters on the board
+                "E", "F", "G", "H", "I", "J" };
+
+            bool valid = false;
+            string pos1;
+            int pos2 = 0;
+
+            // first capitalize their input and trim it
+            position = position.ToUpper();
+            position.Trim();
+            //Console.WriteLine($"Pos: {position}");
+
+            // now check that the letter is from A - Z and in the first position
+            pos1 = position.Substring(0, 1);
+            foreach (string letter in letters)
+            {
+                // if it matches a letter in the array, then it's valid so far
+                if (pos1.Equals(letter))
+                {
+                    valid = true;
+                    //Console.WriteLine($"Valid letter.");
+                    break;
+                }
+
+            }
+
+            // now try to parse the number in the second position
+            try
+            {
+                // see if the string is 3 letter long because if so, it might be a 10
+                if (position.Length == 3)
+                {
+                    pos2 = Int32.Parse(position.Substring(1, 2));
+                }
+                else
+                {
+                    pos2 = Int32.Parse(position.Substring(1, 1));
+                }
+                
+            } catch (Exception)
+            {
+                valid = false; // if it can't be parsed, then the input is not valid
+            }
+
+            // now make sure that the second number is in range from 1-10
+            if (valid) // only do it if it's still valid
+            {
+                if (pos2 < 1 || pos2 > 10)
+                {
+                    valid = false;
+                }
+            }
+
+            // return result
+            return valid;
         }
 
     }
