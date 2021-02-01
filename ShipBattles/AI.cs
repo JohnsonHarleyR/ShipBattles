ae95b8e840a9e63a8a1c030c37c978c045126f07
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2021-02-01 18:24:55 -0500

    Added method to shuffle a stack.

--- ShipBattles/AI.cs
+++ ShipBattles/AI.cs
@@ -35,6 +35,7 @@ namespace ShipBattles
             directions.Push("down");
 
             // now shuffle the directions stack
+            Shuffle(directions);
 
             // now set currentDirection to top of stack
 
@@ -131,5 +132,45 @@ namespace ShipBattles
             // TODO If the AI gets a hit, it will start guessing positions around it too
 
         }
+
+        // shuffle a stack
+        public void Shuffle(Stack<string> stack)
+        {
+            // variables
+            Random random = new Random();
+            List<string> list = new List<string>(); // this will be changed to a new stack
+            List<int> nums = new List<int>(); // use random to pull position from list
+            int max = stack.Count; // decreases with each random number
+
+            // add values to nums depending on size of stack stack
+            for (int i = 0; i < stack.Count; i++)
+            {
+                nums.Add(i);
+            }
+
+            var values = stack.ToArray();
+            stack.Clear();
+
+            // test
+            //Console.Write(nums.Count);
+
+            // testing the shuffle method
+            //Console.Write("\nTesting shuffle: ");
+
+            while (nums.Count > 0)
+            {
+                // get random value from list
+                int num = nums[random.Next(0, nums.Count)]; // the next value to add
+                nums.Remove(num); // remove that value from list
+                string value = values[num];
+                // add random value to the stack
+                stack.Push(value);
+
+                //Console.Write($"{value}, "); //testing
+            }
+
+
+        }
+
     }
 }
